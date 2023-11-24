3e1ab4b3b06b49508265f1ffd0051df452665a74
Ari Dev <AriadneFave@hotmail.com>
2023-11-23 18:42:44 -0600

    change hello

--- frontend/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import "@skeletonlabs/skeleton/themes/theme-crimson.css";
-  import "@skeletonlabs/skeleton/styles/skeleton.css";
-</script>
-
-<div class="px-4">
-  <slot />
-</div>
